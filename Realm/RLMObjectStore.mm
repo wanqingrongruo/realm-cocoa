8566106025cb9aeaa420e7ab7c2b6978bcb23c46
Thomas Goyne <tg@realm.io>
2016-06-16 16:13:19 -0700

    Use a separate index for column -> property lookups rather than sorting the properties
    This makes things a bit less weird when constructing a schema manually for the
    dynamic API (since the properties array visible to you will actually match what
    you passed in), is required to support having the array of properties be a
    subset of the table columns, and simplifies handling columns being rearranged a bit.
    
    Also a little faster since it's O(N) rather than O(N log N), but if you have
    enough properties for it to be even remotely noticeable something has gone very
    wrong.

--- Realm/RLMObjectStore.mm
+++ Realm/RLMObjectStore.mm
@@ -378,7 +378,7 @@ RLMObjectBase *RLMCreateObjectInRealmWithValue(RLMRealm *realm, NSString *classN
         object->_row = (*objectSchema.table)[RLMCreateOrGetRowForObject(objectSchema, primaryGetter, createOrUpdate, created)];
 
         // populate
-        NSArray *props = objectSchema.propertiesInDeclaredOrder;
+        NSArray *props = objectSchema.properties;
         for (NSUInteger i = 0; i < array.count; i++) {
             RLMProperty *prop = props[i];
             // skip primary key when updating since it doesn't change
